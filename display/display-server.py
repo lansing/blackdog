cb823c90346d39e70444eef177438db2062e1db0
Max Lansing <lansing@gmail.com>
2021-04-01 19:53:14 -0700

    add random image display

--- display/display-server.py
+++ display/display-server.py
@@ -1,4 +1,7 @@
 import io
+import glob
+import os
+import random
 
 from flask import Flask
 from flask import request
@@ -8,14 +11,37 @@ from colorthief import ColorThief
 from colorthief import MMCQ
 
 
+IMAGES_PARENT = os.environ.get('IMAGES_DIR', '/mnt/SDCARD/Images')
+
+
 inky = Inky()
 saturation = 0.8
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Display Server'
+
+
+global image_queue = []
+
+def refill_image_queue():
+    subdirs = glob.glob(f"{IMAGES_PARENT}/*")
+    subdirs.sort()
+    current = subdirs[-1]
+    image_queue = glob.glob(f"{current}/*")
+
+def get_random_image():
+    if len(image_queue) == 0:
+        refill_image_queue()
+    i = random.randint(0, len(image_queue))
+    image_path = image_queue[i]
+    f = open(image_path)
+    image_data = f.read()
+    image = Image.open(io.BytesIO(image_data))
+    return image
+
 
 def get_color_edges(image):
     width = image.size[0]
@@ -38,15 +64,8 @@ def get_color_edges(image):
 
     return cmap.palette[0]
 
-    
 
-
-@app.route('/imagez', methods=['POST'])
-def image():
-    f = request.files['image']
-    image_data = f.read()
-    image = Image.open(io.BytesIO(image_data))
-
+def display_image():
     display_ratio = 600 / 448
     image_ratio = image.size[0] / image.size[1]
 
@@ -62,7 +81,7 @@ def image():
     fit_image = image.resize((int(new_width), int(new_height)))
 
     bg_color = get_color_edges(fit_image)
-    
+
     left = (600 - fit_image.size[0]) // 2
     top = (448 - fit_image.size[1]) // 2
 
@@ -72,8 +91,23 @@ def image():
     inky.set_image(final_image, saturation=saturation)
     inky.show()
 
+
+
+@app.route('/imagez', methods=['POST'])
+def image():
+    f = request.files['image']
+    image_data = f.read()
+    image = Image.open(io.BytesIO(image_data))
+
+    #display_image(image)
+
+    display_random_image()
+
     return "Done"
 
-    
+
+def display_random_image():
+    image = get_random_image()
+    display_image(image)
 
 
